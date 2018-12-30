c934024633f31bd57201c4804287fa1fdf3c89cd
Inma <itd.toral@gmail.com>
2018-12-30 14:27:27 +0100

    correcciones

--- index.js
+++ index.js
@@ -5,10 +5,21 @@ var cors = require('cors');
 var path = require('path') 
 
 
-var port = (process.env.PORT || 3000);
-var CREDITS_APP_DIR = '/dist/credits-app';
+var PORT = (process.env.PORT || 3000);
+const CREDITS_APP_DIR = '/dist/credits-app';
 var API_BASE_URL = '/api/v1';
-var dbFileName = __dirname + "/credicts.json";
+var dbFileName = __dirname + "/credits.json";
+
+var initialCredits = [
+    { "project_id": "A02", "total": 200000 },
+    { "project_id": "D04", "total": 450000 }
+];
+
+var db = new DataStore({
+    filename: dbFileName,
+    autoload: true
+});
+
 
 
 // init app
@@ -20,11 +31,156 @@ app.get('/', function(req, res) { 
 res.sendFile(path.join(__dirname, CREDITS_APP_DIR, '/index.html')); 
 });
 
-var initialCredits = [
-    { "project_id": "A02", "total": 200000 },
-    { "project_id": "D04", "total": 450000 }
-];
 
+db.find({},(err,credits)=>{
+    if(err){
+        console.error("Error accesing DB");
+        process.exit(1);
+    }else{
+        if(credits.length == 0){
+            console.log("Empty DB, initializaing data...");
+            db.insert(initialCredits);
+        }else{
+            console.log("Loaded DB with "+credits.length+" credits.");
+        }
+           
+    }
+});
+
+/*app.get("/", (req, res) => {
+    res.send("<html><body><h1>My server</h1></body></html>");
+});*/
+
+app.get(API_BASE_URL + "/credits", (req, res) => {
+    // Obtain all credits
+    console.log(Date()+" - GET /credits");
+    
+    db.find({},(err,credits)=>{
+        if(err){
+            console.error("Error accesing DB");
+            res.sendStatus(500);
+        }else{
+            res.send(credits.map((credit)=>{
+                delete credit._id; //modificado
+                return credit;
+            }));
+        }
+    });
+
+});
+
+app.post(API_BASE_URL + "/credits", (req, res) => {
+    // Create a new credict
+    console.log(Date()+" - POST /credits");
+
+    var credit = req.body;
+
+    db.insert(credit);   //modificado
+
+    res.sendStatus(201);
+});
+
+app.put(API_BASE_URL + "/credits", (req, res) => {
+    // Forbidden
+    console.log(Date()+" - PUT /credits");
+
+    res.sendStatus(405);
+});
+
+app.delete(API_BASE_URL + "/credits", (req, res) => {
+    // Remove all contacts
+    console.log(Date()+" - DELETE /credits");
+
+    db.remove({});
+    
+    res.sendStatus(200);
+});
+
+
+app.post(API_BASE_URL + "/credits/:project_id", (req, res) => {
+    // Forbidden
+    console.log(Date()+" - POST /credits");
+
+    res.sendStatus(405);
+});
+
+
+
+app.get(API_BASE_URL + "/credits/:project_id", (req, res) => {
+    // Get a single 
+    var project_id = req.params.project_id;
+    console.log(Date()+" - GET /credits/"+project_id);
+
+    db.find({"project_id": project_id},(err,credits)=>{
+        if(err){
+            console.error("Error accesing DB");
+            res.sendStatus(500);
+        }else{
+            if(credits.length>1){
+                console.warn("Incosistent DB: duplicated name");
+            }
+            res.send(credicts.map((credit)=>{
+                delete credit._id; //modificado
+                return credit;
+            })[0]);
+        }
+    });
+});
+
+
+app.delete(API_BASE_URL + "/credits/:project_id", (req, res) => {
+    // Delete a single contact
+    var project_id = req.params.project_id;
+    console.log(Date()+" - DELETE /credits/"+project_id);
+
+    db.remove({"project_id": project_id},{},(err,numRemoved)=>{
+        if(err){
+            console.error("Error accesing DB");
+            res.sendStatus(500);
+        }else{
+            if(numRemoved>1){
+                console.warn("Incosistent DB: duplicated name");
+            }else if(numRemoved == 0) {
+                res.sendStatus(404);
+            } else {
+                res.sendStatus(200);
+            }
+        }
+    });
+});
+
+app.put(API_BASE_URL + "/credits/:project_id", (req, res) => {
+    // Update contact
+    var project_id = req.params.project_id;
+    var updatedCredit = req.body;
+    console.log(Date()+" - PUT /credits/"+project_id);
+
+    if(project_id != updatedCredit.project_id){
+        res.sendStatus(409);
+        return;
+    }
+
+    db.update({"project_id": project_id},updatedCredit,(err,numUpdated)=>{
+        if(err){
+            console.error("Error accesing DB");
+            res.sendStatus(500);
+        }else{
+            if(numUpdated>1){
+                console.warn("Incosistent DB: duplicated name");
+            }else if(numUpdated == 0) {
+                res.sendStatus(404);
+            } else {
+                res.sendStatus(200);
+            }
+        }
+    });
+});
+
+
+app.listen(PORT);
+
+console.log("Server ready");
+ 
 /*
 // TODO: fake data to be removed when using mongodb
 credits = [
@@ -175,158 +331,3 @@ app.delete(API_BASE_URL + "/credits/:id/expenses/:id", (req, res) => {
 
 });
 */
-
-var db = new DataStore({
-    filename: dbFileName,
-    autoload: true
-});
-
-db.find({},(err,credits)=>{
-    if(err){
-        console.error("Error accesing DB");
-        process.exit(1);
-    }else{
-        if(credits.length == 0){
-            console.log("Empty DB, initializaing data...");
-            db.insert(initialCredits);
-        }else{
-            console.log("Loaded DB with "+credits.length+" credits.");
-        }
-           
-    }
-});
-
-app.get("/", (req, res) => {
-    res.send("<html><body><h1>My server</h1></body></html>");
-});
-
-app.get(API_BASE_URL + "/credits", (req, res) => {
-    // Obtain all credits
-    console.log(Date()+" - GET /credits");
-    
-    db.find({},(err,credicts)=>{
-        if(err){
-            console.error("Error accesing DB");
-            res.sendStatus(500);
-        }else{
-            res.send(credits.map((credit)=>{
-                //delete credits.project_id; //modificado
-                return credicts;
-            }));
-        }
-    });
-
-});
-
-app.post(API_BASE_URL + "/credicts", (req, res) => {
-    // Create a new credict
-    console.log(Date()+" - POST /credicts");
-
-    var contact = req.body;
-
-    db.insert(credicts);   //modificado
-
-    res.sendStatus(201);
-});
-
-app.put(API_BASE_URL + "/credicts", (req, res) => {
-    // Forbidden
-    console.log(Date()+" - PUT /credicts");
-
-    res.sendStatus(405);
-});
-
-app.delete(API_BASE_URL + "/credicts", (req, res) => {
-    // Remove all contacts
-    console.log(Date()+" - DELETE /credicts");
-
-    db.remove({});
-    
-    res.sendStatus(200);
-});
-
-
-app.post(API_BASE_URL + "/credicts/:project_id", (req, res) => {
-    // Forbidden
-    console.log(Date()+" - POST /credicts");
-
-    res.sendStatus(405);
-});
-
-
-
-app.get(API_BASE_URL + "/credicts/:project_id", (req, res) => {
-    // Get a single 
-    var project_id = req.params.project_id;
-    console.log(Date()+" - GET /credicts/"+project_id);
-
-    db.find({"project_id": project_id},(err,credicts)=>{
-        if(err){
-            console.error("Error accesing DB");
-            res.sendStatus(500);
-        }else{
-            if(contacts.length>1){
-                console.warn("Incosistent DB: duplicated name");
-            }
-            res.send(credicts.map((credicts)=>{
-                delete credicts.project_id; //modificado
-                return credicts;
-            })[0]);
-        }
-    });
-});
-
-
-app.delete(API_BASE_URL + "/credicts/:project_id", (req, res) => {
-    // Delete a single contact
-    var project_id = req.params.project_id;
-    console.log(Date()+" - DELETE /credicts/"+project_id);
-
-    db.remove({"project_id": project_id},{},(err,numRemoved)=>{
-        if(err){
-            console.error("Error accesing DB");
-            res.sendStatus(500);
-        }else{
-            if(numRemoved>1){
-                console.warn("Incosistent DB: duplicated name");
-            }else if(numRemoved == 0) {
-                res.sendStatus(404);
-            } else {
-                res.sendStatus(200);
-            }
-        }
-    });
-});
-
-app.put(API_BASE_URL + "/credicts/:project_id", (req, res) => {
-    // Update contact
-    var project_id = req.params.project_id;
-    var updatedContact = req.body;
-    console.log(Date()+" - PUT /credicts/"+project_id);
-
-    if(project_id != updatedCredict.project_id){
-        res.sendStatus(409);
-        return;
-    }
-
-    db.update({"project_id": project_id},updatedCredict,(err,numUpdated)=>{
-        if(err){
-            console.error("Error accesing DB");
-            res.sendStatus(500);
-        }else{
-            if(numUpdated>1){
-                console.warn("Incosistent DB: duplicated name");
-            }else if(numUpdated == 0) {
-                res.sendStatus(404);
-            } else {
-                res.sendStatus(200);
-            }
-        }
-    });
-});
-
-
-app.listen(port);
-
-console.log("Server ready");
- 
