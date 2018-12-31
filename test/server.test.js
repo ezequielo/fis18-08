c7c6294c5545d9125357ed6c1cf77266003eccf1
Inma <itd.toral@gmail.com>
2018-12-31 19:51:05 +0100

    first basic unit test

--- test/server.test.js
+++ test/server.test.js
@@ -1,5 +1,19 @@
 var server = require('../server');
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var sinon = require('sinon');
+//var sinon = require('sinon');
 var expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Credits API', () => {
+    it('/', (done) => {
+        chai.request(server.app)
+            .get('/')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res).to.be.html;
+                done();
+            });
+    });
+}); 
