78f27e6914084d38f6d81d0e5ec00140e5a54b70
ezequiel <ezequiel@ezequiels-MacBook-Pro.local>
2019-01-09 05:07:45 +0100

    Pacts

--- consumerTests/verifyPacts.js
+++ consumerTests/verifyPacts.js
@@ -11,7 +11,7 @@ var port = (process.env.PORT || 3000);
 const opts = {
     providerBaseUrl: 'http://localhost:3000', // where your service will be running during the test, either staging or localhost on CI
     providerStatesSetupUrl: 'http://localhost:3000/test/setup', // the url to call to set up states
-    pactUrls: ['http://localhost:8080/pacts/provider/ProejctService/consumer/creditsClient/latest'], // the pacts to test against
+    pactUrls: ["https://universidaddesevilla.pact.dius.com.au/pacts/provider/projects/consumer/credits/version/1.0.0"], // the pacts to test against
     customProviderHeaders: ['apikey: test']
 }
 
@@ -19,15 +19,23 @@ app.post('/test/setup', (req, res) => {
     const state = req.body.state
     console.log (req.body)
     switch (state) {
-      case 'it has one project':
-        var contact = { name: 'Foo', phone: 777 };
-        /*Contact.deleteMany({}, (err) => {
-            Contact.create(contact, (err) => {
+      case 'it has one credit':
+        var credit = {
+            "_id": "5c2ba7bcaf87bb00121cef7d",
+            "projectId": "A-01", 
+            "created": "2019-01-01T17:47:40.045Z", 
+            "personnelExpenses": 110, 
+            "executionExpenses": 4, 
+            "income": 200, 
+            "total": 86 
+        };
+        Credit.deleteMany({}, (err) => {
+            Credit.create(credit, (err) => {
                 if (err) {
                     console.error(err);
                 }
             });
-        })*/
+        })
         break
     default:
         break
@@ -42,6 +50,7 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     app.listen(port);
     console.log("Server ready!");
+
 
     var ApiKeyStub = sinon.stub(ApiKey, 'findOne');
     ApiKeyStub.yields(null, new ApiKey({user: "test"}));
